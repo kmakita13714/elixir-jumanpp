c05c79a5203bbb118c4260e50183a98d60abe983
Ko-ichi MAKITA <makita-a@nihon-e.co.jp>
2019-11-22 21:00:37 +0900

    - Add detail output format

--- lib/jumanpp.ex
+++ lib/jumanpp.ex
@@ -7,13 +7,26 @@ defmodule Jumanpp do
     defstruct [:surface, :reading, :headword, :classification, :classification_id, :subclassification, :subclassification_id, :utilization_type, :utilization_type_id, :usage, :usage_id, :semantic_info]
   end
 
-  def parse!(text), do: process_output(exec(text))
+  defmodule TokenDetail do
+    defstruct [
+      :morphological_id, :parent_morphological_id,
+      :start_pos, :end_pos,
+      :surface, :representative_notation, :reading, :headword,
+      :classification, :classification_id,
+      :subclassification, :subclassification_id,
+      :utilization_type, :utilization_type_id,
+      :usage, :usage_id,
+      :semantic_info
+    ]
+  end
 
-  defp exec(text) do
+  def parse!(text, is_detail \\ false), do: process_output(is_detail, exec(is_detail, text))
+
+  defp exec(is_detail, text) do
     filepath = "/tmp/#{System.unique_integer}"
     File.write! filepath, text
-    output = "jumanpp #{filepath}"
-              |> to_char_list
+    output = "jumanpp #{filepath}" <> (if is_detail, do: " -s1", else: "")
+              |> to_charlist
               |> :os.cmd
               |> to_string
     File.rm filepath
@@ -21,29 +34,70 @@ defmodule Jumanpp do
     output
   end
 
-  defp process_output(output), do: process_lines(String.split(output, "\n"), [])
+  defp process_output(is_detail, output), do: process_lines(is_detail, String.split(output, "\n"), [])
 
-  defp process_lines(["EOS" | _], tokens), do: tokens
-  defp process_lines([line | lines], tokens), do: process_lines(lines, tokens ++ [parse_line(line)])
+  defp process_lines(is_detail, ["EOS" | _], tokens), do: tokens
+  defp process_lines(is_detail, [line | lines], tokens) do
+    if line =~ ~r/^#/ do
+      process_lines(is_detail, lines, tokens)
+    else
+      process_lines(is_detail, lines, tokens ++ [parse_line(is_detail, line)])
+    end
+  end
 
-  defp parse_line(str) do
-    [surface, reading, headword, classification, classification_id, subclassification, subclassification_id, utilization_type, utilization_type_id, usage, usage_id | semantic_info] = String.split(str, ~r/\s+/)
+  defp parse_line(is_detail, str) do
+    if is_detail do
+      [
+        _beginning,
+        morphological_id, parent_morphological_id,
+        start_pos, end_pos,
+        surface, representative_notation, reading, headword,
+        classification, classification_id,
+        subclassification, subclassification_id,
+        utilization_type, utilization_type_id,
+        usage, usage_id | semantic_info
+      ] = String.split(str, ~r/\t/)
 
-    semantic_info = if is_list(semantic_info), do: String.replace(Enum.join(semantic_info, " "), "\"", ""), else: semantic_info
+      semantic_info = if is_list(semantic_info), do: String.replace(Enum.join(semantic_info, " "), "\"", ""), else: semantic_info
 
-    %Token{
-      surface: surface,
-      reading: reading,
-      headword: headword,
-      classification: classification,
-      classification_id: classification_id,
-      subclassification: subclassification,
-      subclassification_id: subclassification_id,
-      utilization_type: utilization_type,
-      utilization_type_id: utilization_type_id,
-      usage: usage,
-      usage_id: usage_id,
-      semantic_info: semantic_info,
-    }
+      %TokenDetail{
+        morphological_id: morphological_id,
+        parent_morphological_id: parent_morphological_id,
+        start_pos: start_pos,
+        end_pos: end_pos,
+        surface: surface,
+        representative_notation: representative_notation,
+        reading: reading,
+        headword: headword,
+        classification: classification,
+        classification_id: classification_id,
+        subclassification: subclassification,
+        subclassification_id: subclassification_id,
+        utilization_type: utilization_type,
+        utilization_type_id: utilization_type_id,
+        usage: usage,
+        usage_id: usage_id,
+        semantic_info: semantic_info,
+      }
+    else
+      [surface, reading, headword, classification, classification_id, subclassification, subclassification_id, utilization_type, utilization_type_id, usage, usage_id | semantic_info] = String.split(str, ~r/\s+/)
+
+      semantic_info = if is_list(semantic_info), do: String.replace(Enum.join(semantic_info, " "), "\"", ""), else: semantic_info
+
+      %Token{
+        surface: surface,
+        reading: reading,
+        headword: headword,
+        classification: classification,
+        classification_id: classification_id,
+        subclassification: subclassification,
+        subclassification_id: subclassification_id,
+        utilization_type: utilization_type,
+        utilization_type_id: utilization_type_id,
+        usage: usage,
+        usage_id: usage_id,
+        semantic_info: semantic_info,
+      }
+    end
   end
 end
--- test/jumanpp_test.exs
+++ test/jumanpp_test.exs
@@ -18,5 +18,24 @@ defmodule JumanppTest do
     assert token2.headword == "か"
     assert token2.classification == "助詞"
     assert token2.classification_id == "9"
+
+
+    [token1, token2 | _] = tokens = Jumanpp.parse! "行こうか", true
+
+    assert length(tokens) == 2
+
+    assert token1.morphological_id == "1"
+    assert token1.surface == "行こう"
+    assert token1.reading == "いこう"
+    assert token1.headword == "行く"
+    assert token1.classification == "動詞"
+    assert token1.classification_id == "2"
+
+    assert token2.morphological_id == "2"
+    assert token2.surface == "か"
+    assert token2.reading == "か"
+    assert token2.headword == "か"
+    assert token2.classification == "助詞"
+    assert token2.classification_id == "9"
   end
 end
