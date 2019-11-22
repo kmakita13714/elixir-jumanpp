55da049e58e2c892b1717faec2522df5d3d0c4c1
Ko-ichi MAKITA <makita-a@nihon-e.co.jp>
2019-11-22 21:06:18 +0900

    - Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jumanpp.MixProject do
   def project do
     [
       app: :jumanpp,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
